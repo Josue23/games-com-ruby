2193bc693f26513bdf880d32800cb37fb5e5339a
Josué Rodrigues <josuerodrigues@gmail.com>
2015-11-02 15:50:12 -0200

    adicionado email de contato

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,9 +1,9 @@
+#josuerodrigues@gmail.com
 puts "Bem-vindo ao jogo da advinhação "
 puts "Qual é o seu nome? "
 nome = gets
 puts "\n\n\n\n\n\n"
 puts "Começaremos o jogo para você, " + nome
-#puts "Começaremos o jogo para você, " + gets
 puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "Escolhido... que tal advinhar hoje nosso número secreto? "
